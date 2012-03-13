31c3cce49e65adce3489922220ebe554e6cb41af
Lindsay VanderHoff <lindsay.vanderhoff@gmail.com>
2012-03-12 22:53:16 -0400

    Added user functionalities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def logo
-     image_tag("logo.png", alt: "PayFast", class: "round")
-  end
+#   def logo
+#      image_tag("logo.png", alt: "PayFast", class: "round")
+#   end
 
  # Return a title on a per-page basis.
   def title
